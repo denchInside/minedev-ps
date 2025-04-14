2c3187462edc76aa2efb852009a4320fc7b30f4d
denchInside <82607171+denchInside@users.noreply.github.com>
2025-04-14 19:53:21 +0200

    New features and refactoring

--- src/minedev.ps1
+++ src/minedev.ps1
@@ -1,13 +1,46 @@
-### MineDev Command-Line Utility
-### This script automatically downloads the latest tooling available.
+<#
+.SYNOPSIS
+	Runs the dev tool.
+
+.PARAMETER RunScript
+	Execute a build file.
+
+.PARAMETER RunCommand
+	Execute a single build command.
+
+.PARAMETER Update
+	Force the tooling to update right now.
+
+.PARAMETER Version
+	Display current version.
+
+.PARAMETER SetUpdateBranch
+	Change the branch the tooling is being updated from.
+	Currently, two branches are available: 'main' and 'testing'.
+
+.PARAMETER SetUpdateInterval
+	Set how often the tooling checks for updates.
+	Setting it to -1 disables update checks entirely.
+
+.EXAMPLE
+	./minedev.ps1 -RunScript "./build_my_pack.ps1"
+#>
+
 
 param(
+	[string]$RunScript,
+	[string]$RunCommand,
+	
+	[switch]$Update,
+	[switch]$Version,
+	
+	[string]$SetUpdateBranch,
+	[int]$SetUpdateInterval = -2,
+	
 	# Hack: -Fork is set in a subsequent call
 	# ( the script is started in a new shell 
 	# to prevent any global variables overrides )
-	[switch]$Fork,
-	[switch]$Update,
-	[switch]$Testing
+	[switch]$Fork
 )
 
 
@@ -29,6 +62,7 @@ if (-not $Fork) {
 	return
 }
 
+
 ### Powershell enforcements
 $ErrorActionPreference = "Stop"
 $WarningPreference = "Stop"
@@ -41,6 +75,7 @@ function Stringify-DateTime {
 	param([DateTime]$Value)
 	return $Value.ToString([CultureInfo]::InvariantCulture)
 }
+
 function Parse-DateTime {
 	param([string]$Value)
 	return [DateTime]::Parse(
@@ -49,19 +84,13 @@ function Parse-DateTime {
 		[System.Globalization.DateTimeStyles]::AdjustToUniversal
 	)
 }
+
 function No-Output {
 	param([ScriptBlock]$f)
 	$null = & $f
 }
-function Exit-On-Failure {
-    param([ScriptBlock]$f)
-    try {
-        & $f
-    } catch {
-        Write-Host "Error: $($_.Exception.Message)" -ForegroundColor "Red"
-		[Environment]::Exit(1)
-    }
-}function Assert-IsString {
+
+function Assert-IsString {
 	param([string]$Path)
 
 	if ($Path -eq $null) {
@@ -74,23 +103,30 @@ function Exit-On-Failure {
 		throw "Provided string is empty."
 	}
 }
+
 function Safe-Join {
 	foreach ($arg in $args) {
 		Assert-IsString $arg
 	}
 	return Join-Path @args
 }
+
 function New-Directory {
 	param([string]$Path)
 	
 	Assert-IsString $Path
-	$null = New-Item -Path $Path -ItemType Directory -Force -ErrorAction SilentlyContinue
+	$null = New-Item -Path $Path `
+		-ItemType Directory `
+		-Force `
+		-ErrorAction SilentlyContinue
 }
+
 function Get-TempFileName {
 	return Safe-Join `
 		([System.IO.Path]::GetTempPath()) `
 		([System.IO.Path]::GetRandomFileName())
 }
+
 function Exists {
 	foreach ($arg in $args) {
 		Assert-IsString($arg)
@@ -100,6 +136,7 @@ function Exists {
 		return $false
 	}
 }
+
 filter To-Json {
 	$_ | ConvertTo-Json -Depth 10
 }
@@ -107,6 +144,7 @@ filter To-Json {
 filter From-Json {
 	$_ | ConvertFrom-Json -AsHashtable
 }
+
 function Get-GithubLatestRelease {
 	param(
 		[string]$Author,
@@ -117,7 +155,9 @@ function Get-GithubLatestRelease {
 	)
 
 	$apiUrl = "https://api.github.com/repos/$Author/$Repo/releases"
-	$request = Invoke-RestMethod -Uri $apiUrl -Headers @{ "User-Agent" = "PowerShell" }
+	$request = Invoke-RestMethod `
+		-Uri $apiUrl `
+		-Headers @{ "User-Agent" = "PowerShell" }
 	$release = $request |
 		Where-Object { $_.name -like $NamePattern } |
 		Where-Object { $_.tag_name -like $TagPattern } |
@@ -127,6 +167,7 @@ function Get-GithubLatestRelease {
 	return $release
 }
 
+
 ### Variable declarations
 $MD_ROOT = Safe-Join `
 	([Environment]::GetFolderPath([Environment+SpecialFolder]::ApplicationData)) `
@@ -135,50 +176,94 @@ $MD_REPO_DIR = Safe-Join $MD_ROOT "repo"
 $MD_SCRIPTS_DIR = Safe-Join $MD_REPO_DIR "scripts"
 $MD_BINARIES_DIR = Safe-Join $MD_REPO_DIR "binaries"
 $MD_INFO_FILE = Safe-Join $MD_ROOT "minedev.json"
+$DATE_MIN = Stringify-DateTime ([DateTime]::MinValue)
 
 
 ### Prepare directories
 New-Directory $MD_ROOT
 
+
 ### Load config file
 $MD_CONFIG = $null
+# FIXME: Does not trigger again when interrupted first time and interval is -1
 $FIRST_START = $false
 try {
 	$MD_CONFIG = Get-Content $MD_INFO_FILE -Raw | From-Json
 } catch {
-	$dtDefault = Stringify-DateTime ([DateTime]::MinValue)
 	$MD_CONFIG = @{
-		last_update_date = $dtDefault
-		last_version_date = $dtDefault
+		last_update_date = $DATE_MIN
+		last_version_date = $DATE_MIN
+		update_interval_days = 3
+		update_branch = "main"
 	}
 	$FIRST_START = $true
 }
 
+
 ### Program-related functions
-function Check-For-Updates {
-	Write-Host "### Checking for updates"
-	$pattern = if ($Testing) { "testing-*" } else { "main-*" }
+function Minedev-Exit {
+	param([int]$exitCode)
 	
+	$MD_CONFIG | To-Json > $MD_INFO_FILE
+	exit $exitCode
+}
+
+function Exit-On-Error {
+    param([ScriptBlock]$f)
+    try {
+        & $f
+    } catch {
+        Write-Host "Error: $($_.Exception.Message)" -ForegroundColor "Red"
+		Minedev-Exit 1
+    }
+}
+
+function Check-For-Updates {
+	if (-not $Update) {
+		$tsUpdateInterval = `
+			[TimeSpan]::FromDays($MD_CONFIG.update_interval_days)
+		$tsDaysNotUpdated = `
+			[DateTime]::UtcNow - (Parse-DateTime $MD_CONFIG.last_update_date)
+		if ($tsUpdateInterval -lt 0) {
+			return
+		}
+		if ($tsDaysNotUpdated -lt $tsUpdateInterval) {
+			return
+		}
+	}
+
+	Write-Host "### Checking for updates"
+
 	$release = Get-GithubLatestRelease `
 		-Author "denchInside" `
 		-Repo "minedev-ps" `
-		-TagPattern $pattern `
+		-TagPattern "$($MD_CONFIG.update_branch)-*" `
 		-NamePattern '*' `
 		-PreRelease
 	
-	if (-not $release) {
-		Write-Host "Error: No matching release found." `
-			"Try to run this command with flag '-Testing'." `
+	if ($release -eq $null) {
+		$branch = $MD_CONFIG.update_branch
+		Write-Host "Error: No matching releases found in '$branch'." `
+			"Try changing the update branch via '-SetUpdateBranch'." `
 			-ForegroundColor "Red" `
 			-Separator "`n"
-		if ($FIRST_START) { exit 1 }
+		if ($FIRST_START) { Minedev-Exit 1 }
+		return
+	}
+	
+	if ($Update -or $MD_CONFIG.last_update_tag -ne $release.tag_name) {
+		$MD_CONFIG.last_update_tag = $release.tag_name
+		$MD_CONFIG.last_update_name = $release.name
+	} else {
 		return
 	}
 	
 	$tmpZip = Get-TempFileName
 	$tmpDir = Get-TempFileName
 	No-Output {
-		Remove-Item -Recurse -ErrorAction SilentlyContinue -LiteralPath $MD_REPO_DIR
+		Remove-Item -Recurse `
+			-ErrorAction SilentlyContinue `
+			-LiteralPath $MD_REPO_DIR
 		New-Directory $MD_REPO_DIR
 
 		Invoke-WebRequest $release.zipball_url -OutFile $tmpZip
@@ -199,20 +284,60 @@ function Check-For-Updates {
 		Remove-Item -ErrorAction SilentlyContinue -LiteralPath $tmpZip
 		Remove-Item -Recurse -ErrorAction SilentlyContinue -LiteralPath $tmpDir
 	}
-	$MD_CONFIG.last_update_date = Stringify-DateTime ([DateTime]::UtcNow)
+	$MD_CONFIG.last_version_date = `
+		Stringify-DateTime (Parse-DateTime $release.published_at)
+	$MD_CONFIG.last_update_date = `
+		Stringify-DateTime ([DateTime]::UtcNow)
 }
 
 
-if (-not $Update) {
-	$tsUpdateInterval = [TimeSpan]::FromDays(3)
-	$tsDaysNotUpdated = [DateTime]::UtcNow `
-		- (Parse-DateTime $MD_CONFIG.last_update_date)
-	$Update = $Update -or ($tsDaysNotUpdated -gt $tsUpdateInterval)
+### Version mode
+if ($Version -and $FIRST_START) {
+	Write-Host `
+		"Tooling version not identified yet." `
+		"Try running this command with flag -Update first." `
+		-Separator "`n"
+	Minedev-Exit 0
 }
-if ($Update) {
-	Check-For-Updates
+
+if ($Version) {
+	Write-Host `
+		"Minedev tooling, last updated on $($MD_CONFIG.last_version_date)," `
+		"Release '$($MD_CONFIG.last_update_name)' on branch '$($MD_CONFIG.update_branch)'." `
+		-Separator "`n"
+	Minedev-Exit 0
 }
 
 
-### Save config file
-$MD_CONFIG | To-Json > $MD_INFO_FILE
+### SetUpdateInterval mode
+if ($SetUpdateInterval -gt -2) {
+	$MD_CONFIG.update_interval_days = `
+		Exit-On-Error { [int]$SetUpdateInterval }
+	Write-Host "Interval set to" $SetUpdateInterval
+	Minedev-Exit 0
+}
+
+
+### SetUpdateBranch mode
+if (-not ([string]::IsNullOrEmpty($SetUpdateBranch))) {
+	if ($SetUpdateBranch -in @("main", "testing")) {
+		$MD_CONFIG.update_branch = $SetUpdateBranch.ToLowerInvariant()
+		$MD_CONFIG.last_update_date = $DATE_MIN
+	} else {
+		Write-Host `
+			"Error: invalid branch specified." `
+			"Select either 'main' or 'testing'." `
+			-ForegroundColor "Red" `
+			-Separator "`n"
+		Minedev-Exit 1
+	}
+	
+	Write-Host "Update branch changed to" $MD_CONFIG.update_branch
+	Minedev-Exit 0
+}
+
+### Check for updates
+Check-For-Updates
+
+### Finalize exit
+Minedev-Exit 0
